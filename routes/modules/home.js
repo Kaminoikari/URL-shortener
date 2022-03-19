3f460bf34ca423dc35f809ac85447df8bf4b34b0
Charles <charles0527@hotmail.com>
2022-03-20 04:16:23 +0800

    feat: create generator file

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,11 +1,22 @@
 const express = require('express')
+const url = require('../../models/url')
+const generator = require('./generator')
 const router = express.Router()
 
 
+// Show Home Page
 router.get('/', (req, res) => {
     res.render('index')
 })
 
+// Send Original URL and receive short URL
+router.post('/', (req, res) => {
+    if (!req.body.url)
+        return res.redirect('/')
+    const shortURL = generator(5)
+
+    URL.findOne()
+})
 
 
 
